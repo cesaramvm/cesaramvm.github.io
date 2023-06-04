e1d2dd2a638262ca7aea2891dd7573a322a40127
César Valdés <cvaldesmar@gmail.com>
2023-06-04 16:52:40 +0200

    .

--- scripts.js
+++ scripts.js
@@ -1,40 +1,14 @@
 
 $(document).ready(
-
     function () {
-
-        $('.sideNav>.nav>.nav-item').click(function () {
-            console.log("HOLAAA")
-            if (!($(this).hasClass('active'))) {
-
-                var $this = $(this)
-                var curActive = $this.parent().find('.active')
-                var curPos = $this.parent().children().index(curActive)
-                var nextPos = $this.parent().children().index($this)
-                var lastItem = $(this).parent().children().length - 1;
-                console.log(curPos + "-" + nextPos)
-
-                updateNavs(nextPos);
-                updateContent(curPos, nextPos, lastItem);
-
-            }
-
-        });
-
-
-
         function updateNavs(nextPos) {
             $('nav').children().removeClass('active');
             $('nav').children().eq(nextPos).addClass('active');
         }
 
-
-        // update main content area
         function updateContent(curPos, nextPos, lastItem) {
-
             $('.mainContent').children().removeClass('is-visible');
             $('.mainContent').children().eq(nextPos).addClass('is-visible');
-
             if (nextPos !== 0 && nextPos !== lastItem) {
                 $('.header--cta').addClass('is-active');
             }
@@ -44,9 +18,24 @@ $(document).ready(
 
         }
 
+        $('.nav>.nav-item').click(function () {
+            console.log(".nav>.nav-item")
+            if (!($(this).hasClass('active'))) {
+                var $this = $(this)
+                var curActive = $this.parent().find('.active')
+                var curPos = $this.parent().children().index(curActive)
+                var nextPos = $this.parent().children().index($this)
+                var lastItem = $(this).parent().children().length - 1;
+                updateNavs(nextPos);
+                updateContent(curPos, nextPos, lastItem);
+
+            }
+
+        });
+
 
         $('.toggle').click(function () {
-            console.log("HEY")
+            console.log(".toggle")
 
             $('.perspective').addClass('perspective--modalview');
             setTimeout(function () {
@@ -56,16 +45,16 @@ $(document).ready(
 
         });
 
-        $('.outNav>.nav-item').click(function(){
+        $('.outNav>.nav-item').click(function () {
+            console.log(".outNav>.nav-item")
 
             $('.perspective').removeClass('effect-rotate-left--animate');
-            setTimeout(function(){
-              $('.perspective').removeClass('perspective--modalview');
+            setTimeout(function () {
+                $('.perspective').removeClass('perspective--modalview');
             }, 400);
-            
+
             $('.outNav').removeClass('is-visible');
-      
-          });
+        });
 
 
     });
