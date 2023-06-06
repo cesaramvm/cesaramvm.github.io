b205c5af4f54c74084853520c7bffe442a4fd4d3
César Valdés <cvaldesmar@gmail.com>
2023-06-06 12:53:12 +0200

    -

--- scripts.js
+++ scripts.js
@@ -1,44 +1,22 @@
 
 $(document).ready(
     function () {
-        perspectiveMode = 0;
-        currentBreackpoint = bootstrapDetectBreakpoint()["name"]
-        console.log("Current BreakPoint: " + currentBreackpoint)
-        bootstrapBreakpoints = ["xs","sm","md","lg","xl","xxl"]
-        
-        $( window ).on( "resize", function() {
-            newBreakpoint = bootstrapDetectBreakpoint()["name"]
-            if(currentBreackpoint != newBreakpoint){
-                console.log(currentBreackpoint = newBreakpoint)
-                if (bootstrapBreakpoints.indexOf(currentBreackpoint)<2 && perspectiveMode){
-                    backToNormalPerspective();
-                    setTimeout(function () {
-                        $('.dropdown-toggle').click();
-                    }, 500);
-                }
-            }
-          } );
-          
         function changeArticle(curPos, nextPos) {
             updateNavs(nextPos);
             updateContent(curPos, nextPos);
         }
-
         function updateNavs(pos) {
             activeMenuOption(".dropdown-menu", pos);
             activeMenuOption(".outNav", pos);
         }
-
         function activeMenuOption(menuClass, pos) {
             $(menuClass).children().removeClass('active');
             $(menuClass).children().eq(pos).addClass('active');
         }
-
         function updateContent(curPos, nextPos) {
             makeContentInvisible(".mainContent", curPos);
             makeContentVisible(".mainContent", nextPos);
         }
-
         function changeContentClasses(classSelector, pos, claseAdd, claseRemove) {
             $(classSelector).children().eq(pos).addClass(claseAdd);
             $(classSelector).children().eq(pos).removeClass(claseRemove);
@@ -49,7 +27,6 @@ $(document).ready(
         function makeContentInvisible(classSelector, pos) {
             changeContentClasses(classSelector, pos, "d-none", "d-flex")
         }
-
         function backToNormalPerspective() {            
             $('.outNavContainer').addClass('d-none');
             $('.outNavContainer').removeClass('d-block');
@@ -65,7 +42,6 @@ $(document).ready(
 
             perspectiveMode = 0;
         }
-
         function goToPerspective() {
             $('.perspective').addClass('perspective--modalview');
             setTimeout(function () {
@@ -87,6 +63,21 @@ $(document).ready(
 
         }
 
+        // Redimensión de la ventana
+        $(window).on("resize", function() {
+            newBreakpoint = bootstrapDetectBreakpoint()["name"]
+            if(currentBreackpoint != newBreakpoint){
+                console.log(currentBreackpoint = newBreakpoint)
+                if (bootstrapBreakpoints.indexOf(currentBreackpoint)<2 && perspectiveMode){
+                    backToNormalPerspective();
+                    setTimeout(function () {
+                        $('.dropdown-toggle').click();
+                    }, 500);
+                }
+            }
+            $(".bs-breakpoint").text(currentBreackpoint)
+        });
+        // Gestión de click en menús
         $('.nav>.nav-item, .dropdown-menu>.dropdown-item').click(function () {
             if (!($(this).hasClass('active'))) {
                 var $this = $(this)
@@ -95,44 +86,38 @@ $(document).ready(
                 var nextPos = $this.parent().children().index($this)
                 changeArticle(curPos, nextPos)
             }
-        });
-
+        });     
         $('.toggle').click(function () {
             goToPerspective();
         });
-
+        $('.back-btn').click(function () {
+            backToNormalPerspective();
+        });   
+        // Click en outer menu
         $('.outNav>.nav-item').click(function () {
-            
             document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1.05) translateX(2%)";
-
             setTimeout(function () {
                 document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1)";
             }, 500);
         });
 
-        $('.back-btn').click(function () {
-            backToNormalPerspective();
-        });
-
-        $('.all-fluid').hover(function () {
-            if(perspectiveMode){
-                document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1.2) translateX(7%)";
-            };
-        }, function () {
-            if(perspectiveMode){
-                    document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1)";
-            };
-
-        });
-
+        $('.all-fluid').hover(
+            () => {if (perspectiveMode) document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1.2) translateX(7%)"}, 
+            () => {if (perspectiveMode) document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1)"}
+        );
 
         $('.all-fluid').click(function () {
             if(perspectiveMode){
-                document.getElementsByClassName("effect-rotate-left--animate")[0].style.transform = "scale(1)";
+                setTimeout(()=> document.getElementsByClassName("perspective")[0].style.transform = "scale(1)", 100);
                 backToNormalPerspective();
             };
         });
 
+        perspectiveMode = 0;
+        currentBreackpoint = bootstrapDetectBreakpoint()["name"]
+        console.log("Current BreakPoint: " + currentBreackpoint)
+        bootstrapBreakpoints = ["xs","sm","md","lg","xl","xxl"]
+        $(".bs-breakpoint").text(currentBreackpoint)
         changeArticle(0, 0);
     });
 
